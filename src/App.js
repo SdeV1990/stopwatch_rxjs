b0a838b408129c6ac6e70d1db16e9dc10f9a22b5
SdeV1990 <sapemiaen@gmail.com>
2021-07-13 21:46:45 +0300

    First try...

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// Components
+import Stopwatch from './components/stopwatch/Stopwatch'
+
+const App = () => {
+    return (
+        <Stopwatch/>
+    )
+}
+
+export default App;
